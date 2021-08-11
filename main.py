7b0b8536dfccc6ca822a3a1ed4e14bd4bf2fce75
molijuly <3258594@163.com>
2021-08-11 21:00:39 +0800

    Update from my HIT project.

--- main.py
+++ main.py
@@ -13,9 +13,70 @@ from matplotlib import cm
 from sklearn.manifold import TSNE
 from sklearn.metrics import roc_curve
 
-from device_info import get_nvidia_smi,get_disk_usage
+import subprocess
+
+def pixel_values_in_mask(true_vessels, pred_vessels, masks, split_by_img=False):
+    if split_by_img:
+        n = pred_vessels.shape[0]
+        return (np.array([true_vessels[i, ...][masks[i, ...] == 1].flatten() for i in range(n)]),
+                np.array([pred_vessels[i, ...][masks[i, ...] == 1].flatten() for i in range(n)]))
+    else:
+        return true_vessels[masks == 1].flatten(), pred_vessels[masks == 1].flatten()
+
+def get_gpu_memory_map():
+    """Get the current gpu usage.
+
+    Returns
+    -------
+    usage: dict
+        Keys are device ids as integers.
+        Values are memory usage as integers in MB.
+    """
+    result = subprocess.check_output(
+        [
+            'nvidia-smi', '--query-gpu=memory.free',
+            '--format=csv,nounits,noheader'
+        ])
+    # Convert lines into a dictionary
+    result=result.decode('utf-8')
+    gpu_memory = [int(x) for x in result.strip().split('\n')]
+    gpu_memory_map = dict(zip(range(len(gpu_memory)), gpu_memory))
+    return gpu_memory_map
+
+def get_gpu_temperature_map():
+    result = subprocess.check_output(
+        [
+            'nvidia-smi', '--query-gpu=temperature.gpu',
+            '--format=csv,nounits,noheader'
+        ])
+    # Convert lines into a dictionary
+    result = result.decode('utf-8')
+    gpu_temperature = [int(x) for x in result.strip().split('\n')]
+    gpu_temperature_map = dict(zip(range(len(gpu_temperature)), gpu_temperature))
+    return gpu_temperature_map
+
+def get_gpu_name():
+    result = subprocess.check_output(
+        [
+            'nvidia-smi', '-L'
+        ]
+    )
+    return result.decode('utf-8')
+
+def get_nvidia_smi():
+    result=subprocess.check_output([
+        'nvidia-smi'
+    ])
+    return result.decode('utf-8')
+
+def get_disk_usage():
+    result=subprocess.check_output([
+        'df','-h','/etc'
+    ])
+    return result.decode('utf-8')
 
 
+# TODO: make all functions avaliable in visdomE, especially on net part.
 ### Type aliases for commonly-used types.
 # For optional 'options' parameters.
 # The options parameters can be strongly-typed with the proposed TypedDict type once that is incorporated into the standard.
@@ -41,14 +102,6 @@ could automatically transform the channel order like CHW to HWC.
 In the future I will develope more characters like performance analyze heatmap and tsne. 
 '''
 #use_incoming_socket=False
-def pixel_values_in_mask(true_vessels, pred_vessels, masks, split_by_img=False):
-    if split_by_img:
-        n = pred_vessels.shape[0]
-        return (np.array([true_vessels[i, ...][masks[i, ...] == 1].flatten() for i in range(n)]),
-                np.array([pred_vessels[i, ...][masks[i, ...] == 1].flatten() for i in range(n)]))
-    else:
-        return true_vessels[masks == 1].flatten(), pred_vessels[masks == 1].flatten()
-    
 class Visdom_E(Visdom):
     def __init__(
             self,env=None
@@ -108,6 +161,7 @@ class Visdom_E(Visdom):
 
         if not noAdd:
             self.clockDict[name]['clock'] += 1
+             
 
     def tsne(self,
              batchXchannels,
@@ -169,6 +223,18 @@ class Visdom_E(Visdom):
 
         if not noAdd:
             self.clockDict[name]['clock'] += 1
+            
+    def matplotE(self,
+        mat,
+        name:str,
+        noAdd=False
+    ):
+        if self.clockDict[name]['clock'] == 0:
+            self.clockDict[name]['win'] = self.matplot(mat,env=self.clockDict[name]['env'])
+        else:
+            self.matplot(mat,win=self.clockDict[name]['win'],env=self.clockDict[name]['env'])
+        if not noAdd:
+            self.clockDict[name]['clock'] += 1
 
     def gpuDisk(
             self,
@@ -192,6 +258,27 @@ class Visdom_E(Visdom):
         if not noAdd:
             self.clockDict[name]['clock'] += 1
 
+    def textE(
+            self,
+            data:dict,
+            name: str,
+            noAdd=False
+    ):
+        display=""
+        for key in data:
+            display+="%s "%key
+            display+=str(data[key])
+            display+='<br>'
+
+        if self.clockDict[name]['clock'] == 0:
+            self.clockDict[name]['win'] = super().text(display,env=self.clockDict[name]['env'])
+
+        else:
+            super().text(display,win=self.clockDict[name]['win'],env=self.clockDict[name]['env'])
+
+        if not noAdd:
+            self.clockDict[name]['clock'] += 1
+    
     #add measure functions
     def PRCurve(self,
                 precision:np.ndarray,
@@ -287,11 +374,211 @@ class Visdom_E(Visdom):
         if not noAdd:
             self.clockDict[visName]['clock'] += 1
 
+    def plot_confusion_matrix(self,name,cm,
+                          target_names,
+                          title='Confusion matrix',
+                          cmap=None,
+                          normalize=True,
+                          ):
+        """
+        given a sklearn confusion matrix (cm), make a nice plot
+
+        Arguments
+        ---------
+        cm:           confusion matrix from sklearn.metrics.confusion_matrix
+
+        target_names: given classification classes such as [0, 1, 2]
+                    the class names, for example: ['high', 'medium', 'low']
+
+        save_path:    path to save image.
+
+        title:        the text to display at the top of the matrix
+
+        cmap:         the gradient of the values displayed from matplotlib.pyplot.cm
+                    see http://matplotlib.org/examples/color/colormaps_reference.html
+                    plt.get_cmap('jet') or plt.cm.Blues
+
+        normalize:    If False, plot the raw numbers
+                    If True, plot the proportions
+
+        Usage
+        -----
+        plot_confusion_matrix(cm           = cm,                  # confusion matrix created by
+                                                                # sklearn.metrics.confusion_matrix
+                            normalize    = True,                # show proportions
+                            target_names = y_labels_vals,       # list of names of the classes
+                            title        = best_estimator_name) # title of graph
+
+        Citiation
+        ---------
+        http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
+
+        plot_confusion_matrix(cm           = np.array([[ 1098,  1934,   807],
+                                                [  604,  4392,  6233],
+                                                [  162,  2362, 31760]]),
+                        normalize    = False,
+                        target_names = ['high', 'medium', 'low'],
+                        title        = "Confusion Matrix")
+
+        """
+        import matplotlib
+        import matplotlib.pyplot as plt
+        import numpy as np
+        import itertools
+        matplotlib.use('Agg')
+        accuracy = np.trace(cm) / float(np.sum(cm))
+        misclass = 1 - accuracy
+
+        if cmap is None:
+            cmap = plt.get_cmap('Blues')
+
+        plt.figure(figsize=(8, 6))
+        plt.cla()
+        plt.imshow(cm, interpolation='nearest', cmap=cmap)
+        plt.title(title)
+        plt.colorbar()
+
+        if target_names is not None:
+            tick_marks = np.arange(len(target_names))
+            plt.xticks(tick_marks, target_names, rotation=45)
+            plt.yticks(tick_marks, target_names)
+
+        if normalize:
+            cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+
+        thresh = cm.max() / 1.5 if normalize else cm.max() / 2
+        for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+            if normalize:
+                plt.text(j, i, "{:0.4f}".format(cm[i, j]),
+                        horizontalalignment="center",
+                        color="white" if cm[i, j] > thresh else "black")
+            else:
+                plt.text(j, i, "{:,}".format(cm[i, j]),
+                        horizontalalignment="center",
+                        color="white" if cm[i, j] > thresh else "black")
+
+
+        plt.tight_layout()
+        plt.ylabel('True label')
+        plt.xlabel('Predicted label\naccuracy={:0.4f}; misclass={:0.4f}'.format(accuracy, misclass))
+        # plt.savefig(save_path)
+        self.matplotE(plt,name)
+    
+    def plot_confusion_matrixs(self,name,cms,
+                          cmap=None,
+                          normalize=True,
+                          ):
+        plot_confusion_matrixs_(cms,cmap=cmap,normalize=normalize)
+        self.matplotE(plt,name)
+
+def plot_confusion_matrixs_(cms,
+                          cmap=None,
+                          normalize=True,
+                          ):
+        """
+        given a sklearn confusion matrix (cm), make a nice plot
+
+        Arguments
+        ---------
+        cms:           multiple confusion matrix from sklearn.metrics.confusion_matrix
+
+        target_names: given classification classes such as [0, 1, 2]
+                    the class names, for example: ['high', 'medium', 'low']
+
+        save_path:    path to save image.
+
+        title:        the text to display at the top of the matrix
+
+        cmap:         the gradient of the values displayed from matplotlib.pyplot.cm
+                    see http://matplotlib.org/examples/color/colormaps_reference.html
+                    plt.get_cmap('jet') or plt.cm.Blues
+
+        normalize:    If False, plot the raw numbers
+                    If True, plot the proportions
+
+        Usage
+        -----
+        plot_confusion_matrix(cm           = cm,                  # confusion matrix created by
+                                                                # sklearn.metrics.confusion_matrix
+                            normalize    = True,                # show proportions
+                            target_names = y_labels_vals,       # list of names of the classes
+                            title        = best_estimator_name) # title of graph
+
+        Citiation
+        ---------
+        http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
+
+        plot_confusion_matrix(cm           = np.array([[ 1098,  1934,   807],
+                                                [  604,  4392,  6233],
+                                                [  162,  2362, 31760]]),
+                        normalize    = False,
+                        target_names = ['high', 'medium', 'low'],
+                        title        = "Confusion Matrix")
+
+        """
+        import matplotlib
+        import matplotlib.pyplot as plt
+        import numpy as np
+        import itertools
+        matplotlib.use('Agg')
+
+        if cmap is None:
+            cmap = plt.get_cmap('Blues')
+
+        t=cms.shape[0]
+
+        plt.figure(figsize=(8*t, 6))
+        plt.cla()
+
+        for i in range(t):
+            cm=cms[i]
+            plt.subplot(1,t,i+1)
+            plt.imshow(cm, interpolation='nearest', cmap=cmap)
+            # plt.title(title)
+            # plt.colorbar()
+
+            # accuracy = np.trace(cm) / float(np.sum(cm))
+            # misclass = 1 - accuracy
+
+            # if target_names is not None:
+                # tick_marks = np.arange(len(target_names))
+                # plt.xticks(tick_marks, target_names, rotation=45)
+                # plt.yticks(tick_marks, target_names)
+
+            if normalize:
+                cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+
+            # thresh = cm.max() / 1.5 if normalize else cm.max() / 2
+            # for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+            #     if normalize:
+            #         plt.text(j, i, "{:0.4f}".format(cm[i, j]),
+            #                 horizontalalignment="center",
+            #                 color="white" if cm[i, j] > thresh else "black")
+            #     else:
+            #         plt.text(j, i, "{:,}".format(cm[i, j]),
+            #                 horizontalalignment="center",
+            #                 color="white" if cm[i, j] > thresh else "black")
+
+
+            
+            # plt.ylabel('True label')
+            # plt.xlabel('Predicted label\naccuracy={:0.4f}; misclass={:0.4f}'.format(accuracy, misclass))
+        # plt.savefig(save_path)
+        plt.tight_layout()
+        # plt.show()
+        return plt
+        # self.matplotE(plt,name)
+
 if __name__=='__main__':
     import time
-    vis=Visdom_E()
-    vis.set('progress',env='main')
-    for i in range(100):
-        vis.progressBar(i,'test','progress')
-        time.sleep(1)
+    # vis=Visdom_E()
+    # vis.set('progress',env='main')
+    # for i in range(100):
+    #     vis.progressBar(i,'test','progress')
+    #     time.sleep(1)
+    import numpy as np
+    a=np.random.randn(9,30,30)
+    plot_confusion_matrixs_(a)
 
